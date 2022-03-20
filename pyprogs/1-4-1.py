267aff53a22b7d2b07e6ffe7a93aa560b5a922b1
Prokhor <39743537+prochorwolochoff@users.noreply.github.com>
2022-03-20 16:52:58 +0300

    Финальчик

--- pyprogs/1-4-1.py
+++ pyprogs/1-4-1.py
@@ -1,5 +1,6 @@
 import numpy as np
-
+import math
+i = 0
 a = list(map(int, input('Введите массив A через пробел: ').split()))
 print('Массив а: ', a)
 b = list(map(int, input('Введите массив B через пробел: ').split()))
@@ -10,13 +11,13 @@ n = len(a)
 per = 0
 t = 1
 area = 0
-if ((len(a) > 3) or (len(b) > 3) or (len(c) > 3)):
-    print('Один или несколько массивов не удовлетворяют условию!')
-    break
-else:
-    for i in range(n):
-        per = a[i] + b[i] + c[i]
-        t = per * sqrt(2)
-        area = sqrt(t * (t-a[i]) * (t - b[i]) * (t - c[i]))
-        print('Периметр = ', per)
-        print('Площадь = ', area)
+for i in range(n):
+        if ((len(a) > 3) or (len(b) > 3) or (len(c) > 3)):
+            print('Один или несколько массивов не удовлетворяют условию!')
+            break
+        else:
+            per = a[i] + b[i] + c[i]
+            t = per * (2 ** 0.5)
+            area = (t * (t - a[i]) * (t - b[i]) * (t - c[i])) ** 0.5
+            print('Периметр = ', per)
+            print('Площадь = ', area)
