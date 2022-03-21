a58b18b3ccdc4b42fda1f4210c1d13c9bc4f1528
Prokhor <39743537+prochorwolochoff@users.noreply.github.com>
2022-03-21 11:56:00 +0300

    Ответ снова верный, версия требует доработки исключений

---
+++ pyprogs/1-6.py
@@ -0,0 +1,19 @@
+import math
+n = int(input('Введите кол-во вычислений: '))
+
+if (n < 0):
+    print('Некорректные данные')
+else:
+    a = float(input('Введите значение А: '))
+    x_1 = float(input('Введите значение x1: '))
+    dx = a/4
+    for i in range(n):
+        if (x_1 < a):
+            y = -((((a ** 2/3) - (x ** 2/3)) ** 1/3) ** 0.5)
+        else:
+            y = math.log(x_1)
+            print('n = ', n)
+            print('x1 = ', x_1)
+            print('y = ', y)
+            print('_________')
+            x_1 += dx
