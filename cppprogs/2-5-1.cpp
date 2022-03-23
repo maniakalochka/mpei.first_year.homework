955fca019cd2c7b8ec4f59daa5ade2c956b57f8c
Prokhor <39743537+prochorwolochoff@users.noreply.github.com>
2022-03-23 12:17:40 +0300

    Работает, но помни, что нумерация идет с 0!

---
+++ cppprogs/2-5-1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int i, j, count;
+    float summ, matrix[2][8];
+    cout << "Ваш массив состоит из 2 строк и 8 колонок!" << endl;
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 8; j++){
+            cout << "массив["<< i << "][" << j << "] = ";
+            cin >> matrix[i][j];
+        }
+    }
+    cout << "Ваш массив: "<< endl;
+
+    for (i = 0; i < 2; i++){
+        for (j = 0; j < 8; j++){
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    //cout << "Столбцы с отр. суммой имеет номер:  " << endl;
+    for (j = 0; j < 8; j++) {
+        summ = 0;
+        for (i = 0; i < 2; i++)
+            summ += matrix[i][j];
+            if (summ < 0){
+                cout << "Столбцы с отр. суммой имеет номер:  " << j << endl;
+                count++;
+            }
+    }
+    cout << "Кол-во столбцов с отр. суммой: " << count << endl;
+
+    cin.sync();
+    cin.get();
+    return 0;
+}
