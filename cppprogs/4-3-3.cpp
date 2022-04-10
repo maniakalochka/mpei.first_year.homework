381376edb333461655f7977251c32ed4082b4195
Prokhor <39743537+prochorwolochoff@users.noreply.github.com>
2022-04-10 16:36:12 +0300

    Finaly

--- cppprogs/4-3-3.cpp
+++ cppprogs/4-3-3.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-    float sred_aryth(float a[10][10]){
+    float sred_aryth(float a[10][10], int *pn, int *pm){
         float s = 0, func = 0, proiz = 0;
-        int n  = 0, m = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < 1; j++){
+
+        for(int i = 0; i < *pn; i++){
+            for(int j = 0; j < *pm; j++){
                 if ((a[i][j]) < 0){
-                    a[i][j] = labs(a[i][j]);
-                    s = 0;
+                    a[i][j] = fabs(a[i][j]);
                 }
             }
         }
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
+        for(int i = 0; i < *pn; i++){
+            for(int j = 0; j < *pm; j++){
                 s += a[i][j];
-                sred_aryth = s / (m * n);
+                func = s / (*pm * *pn);
             }
         }
         return func;
@@ -25,7 +25,6 @@ int main()
 {
     float matrix[10][10], s = 0;
     int i, j, n = 0, m = 0, k = 0;
-
     cout << "Сколько строк в матрице?: ";
     cin >> n;
     cout << "Сколько столбцов в матрице?: ";
@@ -62,7 +61,7 @@ int main()
         cout << "Матрица удовлетворяет условию" << endl;
     }
 
-    cout << sred_aryth(matrix) << endl;
+    cout << sred_aryth(matrix, &n, &m) << endl;
     cin.sync();
     cin.get();
     return 0;
